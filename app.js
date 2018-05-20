8ebd3f51fd0fb238bfe963ef64cf6cb07cef2d45
Siddhesh <siddheshmuley1462@gmail.com>
2018-05-20 01:12:35 -0400

    added contact me form and activated receiving email from the service

--- app.js
+++ app.js
@@ -12,14 +12,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(res,res){
-    res.render('index',{'title':'Welcome'});
+app.get('/',function(req,res){
+    res.render('index',{'title':'Computer Not Working'});
 });
-app.get('/about',function(res,res){
+
+app.get('/about',function(req,res){
     res.render('about',{'title':'About'});
 });
-app.get('/contact',function(res,res){
+
+app.get('/contact',function(req,res){
     res.render('contact',{'title':'Contact Us'});
+});
+
+app.post('/contact/send',function(req,res){
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'sassidd@gmail.com',
+            pass: 'Morningstar00'
+        },
+        tls: {
+          rejectUnauthorized: false
+        }
+    });
+    
+    let mailOptions = {
+        from: 'Siddhesh Muley <sassidd@gmail.com>',
+        to: 'Siddhesh Muley <siddheshmuley1462@gmail.com>',
+        subject: 'TestSubmission',
+        text: 'You have a test submission... from: '+req.body.name+' email: '+req.body.email+' with message: '+req.body.message,
+        html: '<p>You have a test submission...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+    };
+    
+    transporter.sendMail(mailOptions, function(error,info){
+        if(error){
+            console.log(error);
+            res.redirect('/');
+        }
+        else{
+            console.log('Message Sent: '+info.response);
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(3000);
